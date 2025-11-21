1a083802cfd5b3c4e66266e7c255aab7880edeb2
Md Abu Faysal Joy <mdalauddinak5352243@gmail.com>
2025-11-21 10:07:03 +0600

    api url replaced with real and live url

--- src/hooks/useAxiosSecure/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure/useAxiosSecure.jsx
@@ -3,7 +3,7 @@ import useAuth from "../useAuth/useAuth";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://the-ultimate-college-management-web.onrender.com",
 });
 const useAxiosSecure = () => {
   const { logout } = useAuth();
